fdc3d3fd5d8dd068a3a3521ba0bec1508724e4cc
Jared Jones <jkjones.dev@gmail.com>
2016-12-21 22:52:08 -0600

    adding new method of sorting based on the colorSum of the pixel, to run use the -c flag

--- pixelSort.py
+++ pixelSort.py
@@ -4,11 +4,47 @@ from PIL import Image
 def displayImage(image):
 	image.show()
 
-def sortImage(pixels):
-	sortHelper(0, len(pixels) - 1, pixels)
+# sort the pixels by their hue
+def colorSort(pixels):
+	start = 0
+	end = 0;
+	while (end < len(pixels) - 1):
+		print("color sorting")
+		start = getFirstNonBlack(start, pixels)
+		end = getNextBlack(start, pixels)
+
+		if (end < 0):
+			break
+
+		quicksortHelper(start, end, pixels)
+
+		start = end + 1
+
+#get the next pixel with a sum greater than 60
+def getFirstNonBlack(index, pixels):
+	while (rgbsum(pixels[index]) < 60):
+		index += 1
+		if (index >= len(pixels)):
+			return -1
+	return index
+
+# get the next pixel with a sum less than 60
+def getNextBlack(index, pixels):
+	while (rgbsum(pixels[index]) > 60):
+		index += 1
+		if (index >= len(pixels)):
+			return -1
+	return index
+
+# return the sum of the rgb values
+def rgbsum(color):
+	return color[0] + color[1] + color[2]
+
+def quicksortImage(pixels):
+	quicksortHelper(1000, len(pixels) - 1000, pixels)
 
 #quicksort on the image
-def sortHelper(lowIndex, highIndex, pixels):
+def quicksortHelper(lowIndex, highIndex, pixels):
 	low = lowIndex
 	high = highIndex
 	pivot = pixels[lowIndex+(highIndex-lowIndex)/2]
@@ -24,9 +60,9 @@ def sortHelper(lowIndex, highIndex, pixels):
 			high-= 1;
 
 	if (lowIndex < high):
-		sortHelper(lowIndex, high, pixels)
+		quicksortHelper(lowIndex, high, pixels)
 	if (highIndex > low):
-		sortHelper(low, highIndex, pixels)
+		quicksortHelper(low, highIndex, pixels)
 
 #swap helper function for swapping pixels in sort
 def swap(i, j, pixels):
@@ -37,8 +73,9 @@ def swap(i, j, pixels):
 def main(argv):
 	inputfile = ''
 	outputfile = ''
+	colorSorting = False
 	try:
-		opts, args = getopt.getopt(argv, "hi:o:", ["ifile=","ofile="])
+		opts, args = getopt.getopt(argv, "hci:o:", ["ifile=","ofile="])
 	except getopt.GetoptError:
 		print 'test.py -i <inputfile> -o <outputfile>'
 		sys.exit(2)
@@ -46,6 +83,8 @@ def main(argv):
 		if opt == '-h':
 			print 'test.py -i <inputfile> -o <outputfile>'
 			sys.exit()
+		elif opt == "-c":
+			colorSorting = True
 		elif opt in ("-i", "--ifile"):
 			inputfile = arg
 		elif opt in ("-o", "--ofile"):
@@ -53,7 +92,11 @@ def main(argv):
 
 	im = Image.open(inputfile)
 	pixels = list(im.getdata())
-	sortImage(pixels)
+
+	if (colorSorting):
+		colorSort(pixels)
+	else:
+		quicksortImage(pixels)
 	imSorted = Image.new(im.mode, im.size)
 	imSorted.putdata(pixels)
 	imSorted.save(outputfile)
